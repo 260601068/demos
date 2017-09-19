b186282e461571ac1aa3794734d6efb87bfced47
260601068 <260601068@qq.com>
2017-09-19 18:30:53 +0800

    add all

---
+++ springmvc-redis-rabbitmq-mybatis-logback/src/main/java/com/wjl/main/MyController.java
@@ -0,0 +1,19 @@
+package com.wjl.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	
+	private Logger logger=LoggerFactory.getLogger(MyController.class);
+	
+	@RequestMapping("/success")
+	public String toSuccess(){
+		logger.info("I'm {},age is {}","wjl",27);
+		return "success";
+	}
+	
+}
