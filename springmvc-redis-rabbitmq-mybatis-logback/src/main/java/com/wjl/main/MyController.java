48d5e99d27f54cc1aaa43aeec64a0a97d0e8cccb
260601068 <260601068@qq.com>
2017-09-21 17:53:31 +0800

    mod

--- springmvc-redis-rabbitmq-mybatis-logback/src/main/java/com/wjl/main/MyController.java
+++ springmvc-redis-rabbitmq-mybatis-logback/src/main/java/com/wjl/main/MyController.java
@@ -2,11 +2,17 @@ package com.wjl.main;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyController {
+	
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
 	
 	private Logger logger=LoggerFactory.getLogger(MyController.class);
 	
@@ -16,6 +22,10 @@ public class MyController {
 		logger.warn("wjl's warn log");
 		logger.info("I'm {},age is {}","wjl",27);
 		logger.debug("wjl's debug log");
+		
+		rabbitTemplate.convertAndSend("myqueue", "wjl message2");
+		String foo = (String) rabbitTemplate.receiveAndConvert("myqueue");
+		System.out.println("foo: "+foo);
 		return "success";
 	}
 	
